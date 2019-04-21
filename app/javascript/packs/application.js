47c9489f4b5af006125200918815a3b93bf4a9d5
Justin <justinbkay@gmail.com>
2019-04-21 10:08:26 -0600

    add materialize-css

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -6,4 +6,6 @@
 require("@rails/ujs").start()
 require("turbolinks").start()
 require("@rails/activestorage").start()
-require("channels")
+// require("channels")
+import '../stylesheets/application'
+import 'materialize-css/dist/js/materialize'

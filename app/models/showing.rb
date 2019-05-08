c0d3312c1f6515d8474d6a11fe0fe8c2d70a7c16
Justin <justinbkay@gmail.com>
2019-05-07 20:15:02 -0600

    replace where with scope

--- app/models/showing.rb
+++ app/models/showing.rb
@@ -3,4 +3,7 @@ class Showing < ApplicationRecord
   belongs_to :theater
 
   validates :play_date, :showtimes, presence: true
+
+  scope :theater_on_date,
+        ->(theater, date) { where(theater_id: theater.id, play_date: date) }
 end

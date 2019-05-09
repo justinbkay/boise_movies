b22c5e959fdf0ab883c3bfce674cc832beac4efc
Justin <justinbkay@gmail.com>
2019-05-08 20:58:43 -0600

    adding spec to test login

--- spec/features/theater_login_spec.rb
+++ spec/features/theater_login_spec.rb
@@ -7,4 +7,13 @@ RSpec.feature 'Theater management requires login', type: :feature do
     # expect(page).to have_text('Rating Filter:')
     expect(current_path).to eq('/')
   end
+
+  scenario 'User logs in and tries to visit theaters index' do
+    visit '/sessions/new'
+    fill_in('username', with: Rails.application.credentials.admin_user)
+    fill_in('password', with: Rails.application.credentials.admin_password)
+    click_button('Log In')
+    expect(current_path).to eq('/theaters')
+  end
+
 end

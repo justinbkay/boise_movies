c2b84203df46d5175cc731fda8d2f22693ff38fb
Justin <justinbkay@gmail.com>
2019-05-13 20:24:22 -0600

    Make sure the date is in the right timezone in production

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -1,6 +1,6 @@
 class MoviesController < ApplicationController
   def index
-    @movies = Movie.where('showings.play_date': Date.today)
+    @movies = Movie.where('showings.play_date': Date.current)
                    .eager_load(:showings)
                    .order(:title)
   end

a02c190ad1fc5e10d30a024b7b46c4f3c1cb56df
Justin <justinbkay@gmail.com>
2019-05-13 20:28:35 -0600

    switching to Date.current

--- spec/models/showing_spec.rb
+++ spec/models/showing_spec.rb
@@ -4,7 +4,7 @@ RSpec.describe Showing, type: :showing do
   it 'requires a play_date to be valid' do
     theater = create(:theater)
     movie = Movie.new
-    movie.showings << Showing.new(theater: theater, play_date: Date.today, showtimes: ['7:00pm', '9:00pm'])
+    movie.showings << Showing.new(theater: theater, play_date: Date.current, showtimes: ['7:00pm', '9:00pm'])
     showing = movie.showings.first
     expect(showing.valid?).to eq(true)
   end
@@ -12,7 +12,7 @@ RSpec.describe Showing, type: :showing do
   it 'requires showtimes to be valid' do
     theater = create(:theater)
     movie = Movie.new
-    showing = Showing.new(theater: theater, play_date: Date.today)
+    showing = Showing.new(theater: theater, play_date: Date.current)
     expect(showing.valid?).to eq(false)
     expect(showing.errors[:showtimes]).to include("can't be blank")
   end

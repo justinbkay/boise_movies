9c4fd0ce41586915898ba85f82f3fcb049b08206
Justin <justinbkay@gmail.com>
2019-05-14 20:20:38 -0600

    order theaters in showtimes

--- app/models/theater.rb
+++ app/models/theater.rb
@@ -2,4 +2,6 @@ class Theater < ApplicationRecord
   has_many :showings
   has_many :movies, through: :showings
   validates :name, :imdb_name, presence: true
+
+  scope :order_by_name, -> { order(:name) }
 end

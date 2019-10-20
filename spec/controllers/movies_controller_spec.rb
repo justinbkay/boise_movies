6340bde6a7ba571e8d75dbbf8e0606c7a1810fe0
Justin <justinbkay@gmail.com>
2019-10-20 09:01:44 -0600

    enable the controller tests

--- spec/controllers/movies_controller_spec.rb
+++ spec/controllers/movies_controller_spec.rb
@@ -3,11 +3,21 @@ require 'rails_helper'
 RSpec.describe MoviesController, type: :controller do
 
   # rspec bug causing failures
-  # describe "GET #index" do
-  #   it "returns http success" do
-  #     get :index
-  #     expect(response).to have_http_status(:success)
-  #   end
-  # end
+  describe "GET #index" do
+    it "returns http success" do
+      get :index
+      expect(response).to have_http_status(:success)
+    end
+  end
+
+  describe "excorcise private method" do
+    it "counts the number of movies by rating" do
+      movie = create(:movie)
+      movies = [movie]
+      @controller = MoviesController.new
+      count = @controller.instance_eval{ make_count(movies) }
+      expect(count).to eq({"R"=>1})
+    end
+  end
 
 end
--- spec/controllers/theaters_controller_spec.rb
+++ spec/controllers/theaters_controller_spec.rb
@@ -2,25 +2,26 @@ require 'rails_helper'
 
 RSpec.describe TheatersController, type: :controller do
 
-  # describe "GET #index" do
-  #   it "returns http success" do
-  #     get 'index'
-  #     expect(response).to have_http_status(:success)
-  #   end
-  # end
+  describe "GET #index" do
+    it "returns http success" do
+      get :index, params: {}, session: { admin: 1 }
+      expect(response).to have_http_status(:success)
+    end
+  end
 
-  # describe "GET #new" do
-  #   it "returns http success" do
-  #     get new_theater_path
-  #     expect(response).to have_http_status(:success)
-  #   end
-  # end
+  describe "GET #new" do
+    it "returns http success" do
+      get :new, params: {}, session: { admin: 1 }
+      expect(response).to have_http_status(:success)
+    end
+  end
 
-  # describe "GET #edit" do
-  #   it "returns http success" do
-  #     get edit_theater_path(1)
-  #     expect(response).to have_http_status(:success)
-  #   end
-  # end
+  describe "GET #edit" do
+    it "returns http success" do
+      theater = create :theater
+      get :edit, params: { id: theater.id }, session: { admin: 1 }
+      expect(response).to have_http_status(:success)
+    end
+  end
 
 end

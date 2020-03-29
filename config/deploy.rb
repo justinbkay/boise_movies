5ec909f20dcc0228d7fb19b7aede997ee6d371b9
Justin <justinbkay@gmail.com>
2020-03-29 10:27:24 -0600

    try adding code climate to CI build

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,5 +1,5 @@
 # config valid for current version and patch releases of Capistrano
-lock "~> 3.11.0"
+lock "~> 3.12.1"
 
 set :application, "boise_movies"
 set :repo_url, "git@github.com:justinbkay/boise_movies.git"
